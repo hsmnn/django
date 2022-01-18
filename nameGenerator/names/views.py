b85a307e88e470f556b981da58c3efb365cf91d1
hsmnn <elie.hausmann@gmail.com>
2022-01-18 15:58:32 +0100

    cookie storage set

--- nameGenerator/names/views.py
+++ nameGenerator/names/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import redirect, render
 from django.views.generic.base import View
 from wonderwords import RandomWord
-from .models import Name
 
 class IndexView(View):
     def index(request):
-        latestName = Name.objects.last()
-        return render(request, 'names/index.html', {'name': latestName})
+        context = {}
+        if (request.session.get('name') != ""):
+            context['name'] = request.session.get('name')
+        return render(request, 'names/index.html', context)
 
     def generate(request):
         r = RandomWord()
         adj = r.word(include_parts_of_speech=["adjectives"])
         name = r.word(include_parts_of_speech=["nouns"])
         fullName = adj + " " + name
-        n = Name()
-        n.name = fullName
-        n.save()
+        request.session['name'] = fullName
         return redirect('/')
